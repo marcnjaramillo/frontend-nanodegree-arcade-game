e034300c9d720dc6fceac0e0f15970f127c990c3
Marc Jaramillo <marcjaramillo@Marcs-MacBook-Pro.local>
2016-06-05 14:17:26 -0700

    Add code for levels

--- js/engine.js
+++ js/engine.js
@@ -97,19 +97,44 @@ var Engine = (function(global) {
         player.update();
     }
 
-    /* This function initially draws the "game level", it will then call
-     * the renderEntities function. Remember, this function is called every
-     * game tick (or loop of the game engine) because that's how games work -
-     * they are flipbooks creating the illusion of animation but in reality
-     * they are just drawing the entire screen over and over.
-     */
     function render() {
-        /* This array holds the relative URL to the image used
-         * for that particular row of the game level.
-         */
 
-        var rowImages = [
-                'images/water-block.png',   // Top row is water
+      /* This defines the first level. */
+
+      var levelOneTopRow = [
+            'images/water-block.png', // Row of water
+            'images/stone-block.png', // Stone bridge
+      ],
+
+        row, col;
+
+        for (col = 0; col < 4; col++) {
+          ctx.drawImage(Resources.get('images/water-block.png'), col * 101, row * 83);
+        }
+          ctx.drawImage(Resources.get('images/stone-block.png'), col * 101, row * 83);
+
+      var levelOne = [
+              'images/stone-block.png',   // Row 1 of 3 of stone
+              'images/stone-block.png',   // Row 2 of 3 of stone
+              'images/stone-block.png',   // Row 3 of 3 of stone
+              'images/grass-block.png',   // Row 1 of 2 of grass
+              'images/grass-block.png'   // Row 2 of 2 of grass
+          ],
+
+          numRows = 5,
+          numCols = 5,
+          row, col;
+
+      for (row = 0; row < numRows; row++) {
+          for (col = 0; col < numCols; col++) {
+
+              ctx.drawImage(Resources.get(levelOne[row]), col * 101, row * 83);
+
+          }
+        }
+
+    /* This defines the second level of the game. */
+      var levelTwo = [
                 'images/stone-block.png',   // Row 1 of 3 of stone
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
@@ -117,27 +142,30 @@ var Engine = (function(global) {
                 'images/grass-block.png'   // Row 2 of 2 of grass
             ],
 
-            numRows = 6,
+            numRows = 5,
             numCols = 5,
             row, col;
 
-        /* Loop through the number of rows and columns we've defined above
-         * and, using the rowImages array, draw the correct image for that
-         * portion of the "grid"
-         */
         for (row = 0; row < numRows; row++) {
             for (col = 0; col < numCols; col++) {
-                /* The drawImage function of the canvas' context element
-                 * requires 3 parameters: the image to draw, the x coordinate
-                 * to start drawing and the y coordinate to start drawing.
-                 * We're using our Resources helpers to refer to our images
-                 * so that we get the benefits of caching these images, since
-                 * we're using them over and over.
-                 */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+
+                ctx.drawImage(Resources.get(levelTwo[row]), col * 101, row * 83);
 
             }
           }
+
+        var levelTwoLastRow = [
+              'images/water-block.png', // Row of water
+              'images/stone-block.png', // Stone bridge
+        ],
+
+          row, col;
+
+          for (col = 0; col < 4; col++) {
+            ctx.drawImage(Resources.get('images/water-block.png'), col * 101, row * 83);
+          }
+            ctx.drawImage(Resources.get('images/stone-block.png'), col * 101, row * 83);
+
         renderEntities();
     }
 
@@ -173,7 +201,9 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/Gem Blue.png',
+        'images/selector.png'
     ]);
     Resources.onReady(init);
 
